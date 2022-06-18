f3f56bd95534bc7d020988aaed55b8bed27b97d0
Antonio Kim <antoniok@sfu.ca>
2022-06-18 07:54:10 -0700

    refactor 2.12 Data for countries

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,37 +1,55 @@
 import React, { useState, useEffect } from "react";
+
 import axios from "axios";
 
-const Information = ({ country }) => {
+const DisplayInformation = ({ country }) => {
   return (
     <div>
       <h1>{country.name.common}</h1>
-      <div>capital {country.capital[0]}</div>
+      <div>capital {country.capital}</div>
       <div>area {country.area}</div>
-
-      <h3>languages</h3>
+      <h3>languages:</h3>
       <ul>
-        {Object.values(country.languages).map((language) => (
-          <li>{language}</li>
+        {Object.values(country.languages).map((language, id) => (
+          <li key={id}>{language}</li>
         ))}
       </ul>
-      <img src={country.flags.svg} alt={country.name.common} height={200} />
+      <img src={country.flags.svg} alt={country.name.common} height={150} />
     </div>
   );
 };
 
-const Country = ({ countries }) => {
+const CountryToggle = ({ country }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  const handleClick = () => setShowInfo(!showInfo);
+
+  return (
+    <div>
+      {showInfo ? <DisplayInformation country={country}/> : country.name.common}
+      <button onClick={handleClick}>{showInfo ? "Unshow" : "Show"}</button>
+    </div>
+  );
+};
+
+const DisplayCountries = ({ countries }) => {
   if (countries.length > 10) {
     return "Too many countries";
   } else if (countries.length === 1) {
-    return <Information country={countries[0]} />;
+    return (
+      <div>
+        <DisplayInformation country={countries[0]} />
+      </div>
+    );
   } else {
-    return countries.map((country) => <div>{country.name.common}</div>);
+    return countries.map((country, id) => {
+      return <CountryToggle key={id} country={country} />;
+    });
   }
 };
 
 const App = () => {
   const [countries, setCountries] = useState([]);
-  const [filteredCountries, setFilteredCountries] = useState("");
+  const [searchedCountries, setSearchedCountries] = useState("");
 
   useEffect(() => {
     axios
@@ -39,21 +57,21 @@ const App = () => {
       .then((response) => setCountries(response.data));
   }, []);
 
-  const handleFilteredCountries = (event) => {
-    setFilteredCountries(event.target.value);
+  const handleFilterCountries = (event) => {
+    setSearchedCountries(event.target.value);
   };
 
   const filter = countries.filter((country) =>
-    country.name.common.toLowerCase().includes(filteredCountries.toLowerCase())
+    country.name.common.toLowerCase().includes(searchedCountries.toLowerCase())
   );
 
   return (
     <div>
       <form>
         find countries{" "}
-        <input value={filteredCountries} onChange={handleFilteredCountries} />
+        <input value={searchedCountries} onChange={handleFilterCountries} />
       </form>
-      <Country countries={filter} />
+      <DisplayCountries countries={filter} />
     </div>
   );
 };
