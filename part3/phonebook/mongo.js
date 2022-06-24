220c914c7be45019dd30db12f5d8d9b81da4b757
Antonio Kim <antoniok@sfu.ca>
2022-06-23 23:33:03 -0700

    complete 3.12 command-line database

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,40 @@
+const { notEqual } = require("assert");
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://antoniok:${password}@cluster0.p7r3k.mongodb.net/Phonebook?retryWrites=true&w=majority`;
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+mongoose.connect(url);
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+});
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  person.save().then((result) => {
+    console.log(`added ${result.name} phone ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });  
+}
