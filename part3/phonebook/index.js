b7587b625457452c265f22448a19f1838fd34470
Antonio Kim <antoniok@sfu.ca>
2022-06-23 18:42:49 -0700

    complete 3.11 phonebook full stack

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 morgan.token("data", (req, res) => {
   return JSON.stringify(req.body);
@@ -93,7 +94,7 @@ app.post("/api/persons/", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
