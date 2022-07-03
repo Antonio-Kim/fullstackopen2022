618e94036e575c08efbeb630781695c359ff4a76
Antonio Kim <antoniok@sfu.ca>
2022-07-03 09:53:20 -0700

    complete Blog list expansions, step 1

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -14,65 +14,84 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-test("correct amount of blog posts are returned", async () => {
-  const blogs = await api.get("/api/blogs");
-  expect(blogs.body).toHaveLength(helper.blogs.length);
-});
+describe("GET methods", () => {
+  test("correct amount of blog posts are returned", async () => {
+    const blogs = await api.get("/api/blogs");
+    expect(blogs.body).toHaveLength(helper.blogs.length);
+  });
 
-test("unique identifier property of the blog posts is named id", async () => {
-  const response = await api.get("/api/blogs");
-  const currentBlogs = response.body;
-  currentBlogs.forEach((blog) => {
-    expect(blog.id).toBeDefined();
+  test("unique identifier property of the blog posts is named id", async () => {
+    const response = await api.get("/api/blogs");
+    const currentBlogs = response.body;
+    currentBlogs.forEach((blog) => {
+      expect(blog.id).toBeDefined();
+    });
   });
 });
 
-test("able to add new blog post", async () => {
-  const newBlog = {
-    title: "Another one bites the dust",
-    author: "Queens",
-    url: "none",
-    likes: 100,
-  };
+describe("POST methods", () => {
+  test("able to add new blog post", async () => {
+    const newBlog = {
+      title: "Another one bites the dust",
+      author: "Queens",
+      url: "none",
+      likes: 100,
+    };
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  const response = await Blog.find({});
-  expect(response).toHaveLength(helper.blogs.length + 1);
-  const lastBlog = response[response.length - 1];
-  expect(lastBlog).toMatchObject(newBlog);
+    const response = await Blog.find({});
+    expect(response).toHaveLength(helper.blogs.length + 1);
+    const lastBlog = response[response.length - 1];
+    expect(lastBlog).toMatchObject(newBlog);
+  });
+
+  test("likes are missing from request", async () => {
+    const newBlog = {
+      title: "Another one bites the dust",
+      author: "Queens",
+      url: "none",
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const response = await Blog.find({});
+    expect(response).toHaveLength(helper.blogs.length + 1);
+    const lastBlog = response[response.length - 1];
+    expect(lastBlog).toHaveProperty("likes", 0);
+  });
+
+  test("mising title and url will return 400 status code", async () => {
+    const newBlog = {
+      author: "Queens",
+      likes: 100,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+  });
 });
 
-test("likes are missing from request", async () => {
-  const newBlog = {
-    title: "Another one bites the dust",
-    author: "Queens",
-    url: "none",
-  };
+describe("DELETE methods", () => {
+  test.only("delete a single blog post resource", async () => {
+    const postsAtStart = await Blog.find({});
+    const blogToDelete = postsAtStart[0];
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api.delete(`/api/blogs/${blogToDelete._id}`).expect(204);
 
-  const response = await Blog.find({});
-  expect(response).toHaveLength(helper.blogs.length + 1);
-  const lastBlog = response[response.length - 1];
-  expect(lastBlog).toHaveProperty("likes", 0);
-});
+    const blogsAtEnd = await Blog.find({});
+    expect(blogsAtEnd).toHaveLength(helper.blogs.length - 1);
 
-test("mising title and url will return 400 status code", async () => {
-  const newBlog = {
-    author: "Queens",
-    likes: 100,
-  };
-
-  await api.post("/api/blogs").send(newBlog).expect(400);
+    const content = blogsAtEnd.map((r) => r.title);
+    expect(content).not.toContain(blogToDelete.title);
+  });
 });
 
 afterAll(() => {
