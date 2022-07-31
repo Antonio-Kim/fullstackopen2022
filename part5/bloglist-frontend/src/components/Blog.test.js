4cffd11ed6f05335c5a7adff1835c1c30322d9b9
Antonio Kim <antoniok@sfu.ca>
2022-07-31 03:11:19 -0700

    complete 5.14 Blog list test, step 2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 describe("<Blog />", () => {
@@ -19,5 +20,16 @@ describe("<Blog />", () => {
   test("at start url and likes are not displayed", () => {
     const div = container.querySelector(".blogContent");
     expect(div).toHaveStyle("display: none");
+  });
+
+  test("after clikcing the view button, url and likes are displayed", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByText("view");
+    await user.click(button);
+
+    screen.debug();
+
+    const div = container.querySelector(".blogContent");
+    expect(div).not.toHaveStyle("display: none");
   });
 });
