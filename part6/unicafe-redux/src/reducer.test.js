24be1a37115fc7b7983c7f8e25f0461246d87e40
Antonio Kim <antoniok@sfu.ca>
2022-08-05 09:11:07 -0700

    complete 6.1 unicafe revisited, step 1

---
+++ part6/unicafe-redux/src/reducer.test.js
@@ -0,0 +1,59 @@
+import deepFreeze from "deep-freeze";
+import counterReducer from "./reducer";
+
+describe("unicafe reducer", () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0,
+  };
+
+  test("shoud return a proper initial state when called with undefined state", () => {
+    const state = {};
+    const action = {
+      type: "DO_NOTHING",
+    };
+    deepFreeze(state);
+    const newState = counterReducer(undefined, action);
+    expect(newState).toEqual(initialState);
+  });
+
+  test("good is incremented", () => {
+    const action = {
+      type: "GOOD",
+    };
+    const state = initialState;
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0,
+    });
+  });
+
+  test("bad is incremented", () => {
+    const action = {
+      type: "BAD",
+    };
+    const state = initialState;
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1,
+    });
+
+    test("zero resets the state", () => {
+      const state = {
+        good: 10,
+        okay: 7,
+        bad: 17,
+      };
+      deepFreeze(state);
+      const newState = counterReducer(state, action);
+      expect(newState).toEqual(initialState);
+    });
+  });
+});
