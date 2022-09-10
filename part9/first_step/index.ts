7e6da7119bc54ed7dbd6dcc8be71ab0d3930cbbc
Antonio Kim <antoniok@sfu.ca>
2022-09-10 15:54:51 -0700

    complete 9.7 WebExercises

--- part9/first_step/index.ts
+++ part9/first_step/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import calculateBmi from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
@@ -22,6 +24,26 @@ app.get("/bmi", (req, res) => {
   } else {
     res.status(400).send({ error: "malformatted parameters" });
   }
+});
+
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const body = req.body;
+
+  if (!body.daily_exercises || !body.target) {
+    return res.status(400).json({
+      error: "parameters missing",
+    });
+  }
+
+  if (body.daily_exercises !instanceof Array<String> || body.target !instanceof Number) {
+    return res.status(400).json({
+      error: "malformatted parameters"
+    })
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const result = calculateExercises(body.daily_exercises, body.target);
+  return res.send(result);
 });
 
 const PORT = 3002;
