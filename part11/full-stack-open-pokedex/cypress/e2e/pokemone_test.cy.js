adf0864dfe0233680c7dc96f3a5667f202bddcdf
Antonio <antonio_kim@outlook.com>
2022-10-01 22:38:15 -0700

    complete 11.9 simple end-to-end tests

---
+++ part11/full-stack-open-pokedex/cypress/e2e/pokemone_test.cy.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+describe("Pokedex", function () {
+  it("check front page is open", function () {
+    cy.visit("http://localhost:5000");
+    cy.contains(
+      "Pokémon and Pokémon character names are trademarks of Nintendo."
+    );
+  });
+  it("front page can be opened", function () {
+    cy.visit("http://localhost:5000");
+    cy.contains("ivysaur");
+    cy.contains(
+      "Pokémon and Pokémon character names are trademarks of Nintendo."
+    );
+  });
+
+  it("pokemon page can be navigated to", function () {
+    cy.visit("http://localhost:5000");
+    cy.contains("ivysaur").click();
+    cy.contains("chlorophyll");
+  });
+});
